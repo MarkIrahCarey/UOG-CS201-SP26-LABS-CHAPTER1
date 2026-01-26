0c3e9c33fa12cde0c6bad327fb472ddbd1c47583
MarkIrahCarey <markiragalang1024@gmail.com>
2026-01-26 21:16:20 +1000

    Added more labs, add content on README.md

--- lab2.py
+++ lab2.py
@@ -4,7 +4,6 @@ Date:
 Due Date: Feb 5, 2026
 
 Create a program that prints out the first verse or lyrics of your favorite song (appropriate please)
-
 For now, please use multiple print statements per line
 """
 

2139636140c6bd5a74da3e37e4432d4700847343
MarkIrahCarey <markiragalang1024@gmail.com>
2026-01-26 20:13:52 +1000

    Initial Commit

---
+++ lab1.py
@@ -0,0 +1,12 @@
+"""
+Author:
+Date:
+Due Date: Feb 5, 2026
+
+Create a program that prompts the user asking about their age. Then from there, print out
+a message that says in 5 years, they will be x + 5 years!
+
+So for example, if the user entered 23 after the prompt, they will get a small message:
+"In 5 years, you will be 28!"
+"""
+
